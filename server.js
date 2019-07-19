43be67bde643706b4ba250f7b69e5afec1570ea6
Maysam Rabipour <maysam.rabipour@gmail.com>
2019-07-19 17:10:23 +0430

    initial authors

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+
+
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -14,6 +18,8 @@ app.set('layout', 'layouts/layout')
 
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser : true })
@@ -23,6 +29,8 @@ const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
+
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
